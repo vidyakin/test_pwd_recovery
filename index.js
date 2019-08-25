207883110be27b2bf4df127670a155709b0d1326
Sergey Vidyakin <vidyakin.sergey@gmail.com>
2019-08-25 04:01:26 +0300

    проверка пароля

--- index.js
+++ index.js
@@ -36,7 +36,20 @@ async function populateDemo() {
     console.log('БД была заполнена демо-данными');    
     return await db.collection("passwords").insertMany(demoUsers)
 }
-                
+             
+async function fillDemoDataWhenFirst() {
+    // всегда смотрим в базу и проверяем если ли там пользователи
+    let num = await db.collection('passwords').countDocuments({}) //, (err,num) => {
+    
+    if (num == 0) { // если записей нет, заполняем демо-данными
+        await populateDemo()        
+    }
+    
+    let msg = num == 0 ? 'Обнаружен первый запуск. БД была заполнена демо-данными' : ''
+
+    return msg
+}
+   
 function connectDb() {
     client.connect(err => {
         if (err) {
@@ -52,19 +65,6 @@ function connectDb() {
             }            
         }
     })
-}
-
-async function fillDemoDataWhenFirst() {
-    // всегда смотрим в базу и проверяем если ли там пользователи
-    let num = await db.collection('passwords').countDocuments({}) //, (err,num) => {
-    
-    if (num == 0) { // если записей нет, заполняем демо-данными
-        await populateDemo()        
-    }
-    
-    let msg = num == 0 ? 'Обнаружен первый запуск. БД была заполнена демо-данными' : ''
-
-    return msg
 }
 
 // =========================================
@@ -108,7 +108,7 @@ app.post('/recover', async (req, res)=>{
         expiredAt.setHours(expiredAt.getHours()+1)
         // записываем в базу {user_id, token, expireAt}
         try {
-            let res = await db.collection('tokens').insertOne({ token: data.token, user: req.body.mail, expiredAt})
+            let res = await db.collection('tokens').insertOne({ token: data.token, user: req.body.mail, isActive: true, expiredAt})
             if (!res) {
                 data.msg = 'Ошибка записи токена, не получен результат записи'
                 //res.render('index', data)
@@ -134,13 +134,54 @@ app.post('/recover', async (req, res)=>{
 
 // прием токена по ссылке
 app.get('/recover/:token', (req, res)=>{
-    // выводим страницу с формой ввода нового пароля
-    res.render('newPassword', {token: req.params.token})
-    console.log(`Передан токен ${req.params.token}`); 
+    // найти токен в базе, проверить дату
+    let msg = 'msg init'
+    tokenInfo = await db.collection('tokens').findOne({token: req.params.token})
+    if (tokenInfo) {
+        if (tokenInfo.expiredAt < new Date()) {
+            msg = 'Срок действия ссылки для смены пароля истек, получите новую ссылку'
+            res.render('index', {token: req.params.token, msg})
+        }
+        else { // ВСЕ ОК
+            res.render('newPassword', {token: req.params.token})
+        }
+    }
+    else {
+        msg = 'Ссылка не действительная, токен не найден'
+        res.render('index', {token: req.params.token, msg})
+    }
+    // выводим страницу с формой ввода нового пароля    
+    console.log(`Передан токен ${req.params.token}, Сообщение: ${msg}`); 
 })
 
 // прием формы с новым паролем, проверка, запись нового пароля, авторизация 
 app.post('/confirm', (req, res)=>{
+    // Проверяем пароль на проверки
+    let data = {
+        msg: ''
+    }
+    if (req.params.password != req.params.passwordConfirm) {
+        data.msg = 'Пароли не совпадают, введите повторно'
+    }
+    else if (req.params.password.length < 5) {
+        data.msg = 'Длина пароля должна быть не менее 5 симолов'
+    }
+    else {
+        // находим по токену пользователя и его емейл
+        db.collection('tokens').findOne({token: req.params.token})
+            .then(userInfo => {
+                // записываем новый пароль в пользователя, 
+                db.collection('passwords').update({email: userInfo.email}, {$set: {password: req.params.password}})
+                    .then(r => {
+                        // помечаем токен как использованный
+                        db.collection('tokens').update({token: req.params.token}, {$set: {isActive: false}})
+                            .then(r => {
+                                // логииним через пасспорт
+
+                            })
+                    })
+            }
+    }
     console.log(`Передан новый пароль: ${req.params.password}`)
 })
 
