f66af64a861fe9ebe5712b21f62c9a0ae960ed4e
Sergey Vidyakin <vidyakin.sergey@gmail.com>
2019-08-29 20:33:05 +0300

    правки eslint

--- test_async.js
+++ test_async.js
@@ -1,32 +1,33 @@
 // получение через промис
-function getToken() {
-    return new Promise( (resolve, reject) => {
-        require('crypto').randomBytes(16, (err,buf) => resolve(buf.toString('hex'))) 
-    })
+function getToken () {
+  return new Promise((resolve, reject) => {
+    require('crypto').randomBytes(16, (err, buf) => resolve(buf.toString('hex')))
+  })
 }
 
 // получение через асинк
-async function getToken2() {  // асинхронная "обертка"
-    return await new Promise((rs,rj) => { 
-        require('crypto').randomBytes(16, 
-            (err,buf) => {
-                if (err) rj (err)
-                else 
-                    rs(buf.toString('hex')) // прокидываем в промис
-            }
-        ) 
-    })        
+async function getToken2 () { // асинхронная "обертка"
+  return new Promise((resolve, reject) => {
+    require('crypto').randomBytes(16,
+      (err, buf) => {
+        if (err) reject(err)
+        else {
+          resolve(buf.toString('hex'))
+        } // прокидываем в промис
+      }
+    )
+  })
 }
 
 // асинхронная обертка вызовов чтоб работал await
-(async ()=>{
-    // №1
-    getToken()
-        .then(x => {
-            console.log('TOKEN 1 is:'+ x)
-        })
-    
-    // №2
-    let t = await getToken2()
-    console.log('TOKEN 2 is:'+ t )
+(async () => {
+  // №1
+  getToken()
+    .then(x => {
+      console.log('TOKEN 1 is:' + x)
+    })
+
+  // №2
+  const t = await getToken2()
+  console.log('TOKEN 2 is:' + t)
 })()
